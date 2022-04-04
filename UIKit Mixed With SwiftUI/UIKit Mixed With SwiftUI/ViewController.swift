008a5be87a3b8a6304fde4c79e22c0cac07889e7
João Pedro Soares <joao.p.soares93@gmail.com>
2022-04-04 22:47:42 +0100

    Initial Setup

---
+++ UIKit Mixed With SwiftUI/UIKit Mixed With SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit Mixed With SwiftUI
+//
+//  Created by João Pedro on 04/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
